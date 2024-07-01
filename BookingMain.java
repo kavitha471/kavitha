410d8d693d1b46040b275dc4d57cfca17eda2b9e
kavitha471 <kavithajona169@gmail.com>
2024-07-01 10:18:35 +0530

    Add files via upload

---
+++ BookingMain.java
@@ -0,0 +1,569 @@
+package TheatreBooking;
+import java.util.*;
+public class BookingMain {
+
+	public static void main(String[] args) {
+		
+		List<String>a11 = new ArrayList<>(10);
+		List<String>b11 = new ArrayList<>(10);
+		List<String>c11 = new ArrayList<>(10);
+		List<String>d11 = new ArrayList<>(10);
+		List<String>e11 = new ArrayList<>(10);
+		List<String>f11 = new ArrayList<>(10);
+		List<String>g11 = new ArrayList<>(10);
+		List<String>h11 = new ArrayList<>(10);
+		List<String>j11 = new ArrayList<>(10);
+		//12 not it's 02
+		List<String>a12 = new ArrayList<>(10);
+		List<String>b12 = new ArrayList<>(10);
+		List<String>c12 = new ArrayList<>(10);
+		List<String>d12 = new ArrayList<>(10);
+		List<String>e12 = new ArrayList<>(10);
+		List<String>f12 = new ArrayList<>(10);
+		List<String>g12 = new ArrayList<>(10);
+		List<String>h12 = new ArrayList<>(10);
+		List<String>j12 = new ArrayList<>(10);
+		// 06
+		List<String>a6 = new ArrayList<>(10);
+		List<String>b6 = new ArrayList<>(10);
+		List<String>c6 = new ArrayList<>(10);
+		List<String>d6 = new ArrayList<>(10);
+		List<String>e6 = new ArrayList<>(10);
+		List<String>f6 = new ArrayList<>(10);
+		List<String>g6 = new ArrayList<>(10);
+		List<String>h6 = new ArrayList<>(10);
+		List<String>j6 = new ArrayList<>(10);
+		// 10
+		List<String>a10 = new ArrayList<>(10);
+		List<String>b10 = new ArrayList<>(10);
+		List<String>c10 = new ArrayList<>(10);
+		List<String>d10 = new ArrayList<>(10);
+		List<String>e10 = new ArrayList<>(10);
+		List<String>f10 = new ArrayList<>(10);
+		List<String>g10 = new ArrayList<>(10);
+		List<String>h10 = new ArrayList<>(10);
+		List<String>j10= new ArrayList<>(10);
+		
+		
+		a11.add("1");
+		a11.add("2");
+		a11.add("3");
+		a11.add("4");
+		a11.add("5");
+		a11.add("6");
+		a11.add("7");
+		a11.add("8");
+		a11.add("9");
+		a11.add("10");
+		a11.add("11");
+		a11.add("12");
+		a11.add("13");
+		a11.add("14");
+		a11.add("15");
+		//
+		b11.add("1");
+		b11.add("2");
+		b11.add("3");
+		b11.add("4");
+		b11.add("5");
+		b11.add("6");
+		b11.add("7");
+		b11.add("8");
+		b11.add("9");
+		b11.add("10");
+		b11.add("11");
+		b11.add("12");
+		//
+		c11.add("1");
+		c11.add("2");
+		c11.add("3");
+		c11.add("4");
+		c11.add("5");
+		c11.add("6");
+		c11.add("7");
+		c11.add("8");
+		c11.add("9");
+		c11.add("10");
+		c11.add("11");
+		c11.add("12");
+		//
+		d11.add("1");
+		d11.add("2");
+		d11.add("3");
+		d11.add("4");
+		d11.add("5");
+		d11.add("6");
+		d11.add("7");
+		d11.add("8");
+		d11.add("9");
+		d11.add("10");
+		d11.add("11");
+		d11.add("12");
+		//
+		e11.add("1");
+		e11.add("2");
+		e11.add("3");
+		e11.add("4");
+		e11.add("5");
+		e11.add("6");
+		e11.add("7");
+		e11.add("8");
+		e11.add("9");
+		e11.add("10");
+		e11.add("11");
+		e11.add("12");
+		//
+		f11.add("1");
+		f11.add("2");
+		f11.add("3");
+		f11.add("4");
+		f11.add("5");
+		f11.add("6");
+		f11.add("7");
+		f11.add("8");
+		f11.add("9");
+		f11.add("10");
+		f11.add("11");
+		f11.add("12");
+		//
+		g11.add("1");
+		g11.add("2");
+		g11.add("3");
+		g11.add("4");
+		g11.add("5");
+		g11.add("6");
+		g11.add("7");
+		g11.add("8");
+		g11.add("9");
+		g11.add("10");
+		g11.add("11");
+		g11.add("12");
+		//
+		h11.add("1");
+		h11.add("2");
+		h11.add("3");
+		h11.add("4");
+		h11.add("5");
+		h11.add("6");
+		h11.add("7");
+		h11.add("8");
+		h11.add("9");
+		h11.add("10");
+		h11.add("11");
+		h11.add("12");
+		//
+		j11.add("1");
+		j11.add("2");
+		j11.add("3");
+		j11.add("4");
+		j11.add("5");
+		j11.add("6");
+		j11.add("7");
+		j11.add("8");
+		j11.add("9");
+		j11.add("10");
+		j11.add("11");
+		j11.add("12");
+		//
+		// 12 next 
+		
+		a12.add("1");
+		a12.add("2");
+		a12.add("3");
+		a12.add("4");
+		a12.add("5");
+		a12.add("6");
+		a12.add("7");
+		a12.add("8");
+		a12.add("9");
+		a12.add("10");
+		a12.add("11");
+		a12.add("12");
+		a12.add("13");
+		a12.add("14");
+		a12.add("15");
+		//
+		b12.add("1");
+		b12.add("2");
+		b12.add("3");
+		b12.add("4");
+		b12.add("5");
+		b12.add("6");
+		b12.add("7");
+		b12.add("8");
+		b12.add("9");
+		b12.add("10");
+		b12.add("11");
+		b12.add("12");
+		//
+		c12.add("1");
+		c12.add("2");
+		c12.add("3");
+		c12.add("4");
+		c12.add("5");
+		c12.add("6");
+		c12.add("7");
+		c12.add("8");
+		c12.add("9");
+		c12.add("10");
+		c12.add("11");
+		c12.add("12");
+		//
+		d12.add("1");
+		d12.add("2");
+		d12.add("3");
+		d12.add("4");
+		d12.add("5");
+		d12.add("6");
+		d12.add("7");
+		d12.add("8");
+		d12.add("9");
+		d12.add("10");
+		d12.add("11");
+		d12.add("12");
+		//
+		e12.add("1");
+		e12.add("2");
+		e12.add("3");
+		e12.add("4");
+		e12.add("5");
+		e12.add("6");
+		e12.add("7");
+		e12.add("8");
+		e12.add("9");
+		e12.add("10");
+		e12.add("11");
+		e12.add("12");
+		//
+		f12.add("1");
+		f12.add("2");
+		f12.add("3");
+		f12.add("4");
+		f12.add("5");
+		f12.add("6");
+		f12.add("7");
+		f12.add("8");
+		f12.add("9");
+		f12.add("10");
+		f12.add("11");
+		f12.add("12");
+		//
+		g12.add("1");
+		g12.add("2");
+		g12.add("3");
+		g12.add("4");
+		g12.add("5");
+		g12.add("6");
+		g12.add("7");
+		g12.add("8");
+		g12.add("9");
+		g12.add("10");
+		g12.add("11");
+		g12.add("12");
+		//
+		h12.add("1");
+		h12.add("2");
+		h12.add("3");
+		h12.add("4");
+		h12.add("5");
+		h12.add("6");
+		h12.add("7");
+		h12.add("8");
+		h12.add("9");
+		h12.add("10");
+		h12.add("11");
+		h12.add("12");
+		//
+		j12.add("1");
+		j12.add("2");
+		j12.add("3");
+		j12.add("4");
+		j12.add("5");
+		j12.add("6");
+		j12.add("7");
+		j12.add("8");
+		j12.add("9");
+		j12.add("10");
+		j12.add("11");
+		j12.add("12");
+		//
+		
+		// 6 next
+		
+		a6.add("1");
+        a6.add("2");
+        a6.add("3");
+        a6.add("4");
+        a6.add("5");
+        a6.add("6");
+        a6.add("7");
+        a6.add("8");
+        a6.add("9");
+        a6.add("10");
+        a6.add("11");
+        a6.add("12");
+		a6.add("13");
+		a6.add("14");
+		a6.add("15");
+		//
+			b6.add("1");
+	        b6.add("2");
+	        b6.add("3");
+	        b6.add("4");
+	        b6.add("5");
+	        b6.add("6");
+	        b6.add("7");
+	        b6.add("8");
+	        b6.add("9");
+	        b6.add("10");
+	        b6.add("11");
+	        b6.add("12");
+		//
+		c6.add("1");
+        c6.add("2");
+        c6.add("3");
+        c6.add("4");
+        c6.add("5");
+        c6.add("6");
+        c6.add("7");
+        c6.add("8");
+        c6.add("9");
+        c6.add("10");
+        c6.add("11");
+        c6.add("12");
+		//
+		d6.add("1");
+		d6.add("2");
+		d6.add("3");
+		d6.add("4");
+		d6.add("5");
+		d6.add("6");
+		d6.add("7");
+		d6.add("8");
+		d6.add("9");
+		d6.add("10");
+		d6.add("11");
+		d6.add("12");
+		//
+		e6.add("1");
+		e6.add("2");
+		e6.add("3");
+		e6.add("4");
+		e6.add("5");
+		e6.add("6");
+		e6.add("7");
+		e6.add("8");
+		e6.add("9");
+		e6.add("10");
+		e6.add("11");
+		e6.add("12");
+		//
+		f6.add("1");
+		f6.add("2");
+		f6.add("3");
+		f6.add("4");
+		f6.add("5");
+		f6.add("6");
+		f6.add("7");
+		f6.add("8");
+		f6.add("9");
+		f6.add("10");
+		f6.add("11");
+		f6.add("12");
+		//
+		g6.add("1");
+		g6.add("2");
+		g6.add("3");
+		g6.add("4");
+		g6.add("5");
+		g6.add("6");
+		g6.add("7");
+		g6.add("8");
+		g6.add("9");
+		g6.add("10");
+		g6.add("11");
+		g6.add("12");
+		//
+		h6.add("1");
+		h6.add("2");
+		h6.add("3");
+		h6.add("4");
+		h6.add("5");
+		h6.add("6");
+		h6.add("7");
+		h6.add("8");
+		h6.add("9");
+		h6.add("10");
+		h6.add("11");
+		h6.add("12");
+		//
+		j6.add("1");
+		j6.add("2");
+		j6.add("3");
+		j6.add("4");
+		j6.add("5");
+		j6.add("6");
+		j6.add("7");
+		j6.add("8");
+		j6.add("9");
+		j6.add("10");
+		j6.add("11");
+		j6.add("12");
+		// 10 next
+		a10.add("1");
+        a10.add("2");
+        a10.add("3");
+        a10.add("4");
+        a10.add("5");
+        a10.add("6");
+        a10.add("7");
+        a10.add("8");
+        a10.add("9");
+        a10.add("10");
+        a10.add("11");
+        a10.add("12");
+        a10.add("13");
+        a10.add("14");
+        a10.add("15");
+        //
+        b10.add("1");
+        b10.add("2");
+        b10.add("3");
+        b10.add("4");
+        b10.add("5");
+        b10.add("6");
+        b10.add("7");
+        b10.add("8");
+        b10.add("9");
+        b10.add("10");
+        b10.add("11");
+        b10.add("12");
+        //
+        c10.add("1");
+        c10.add("2");
+        c10.add("3");
+        c10.add("4");
+        c10.add("5");
+        c10.add("6");
+        c10.add("7");
+        c10.add("8");
+        c10.add("9");
+        c10.add("10");
+        c10.add("11");
+        c10.add("12");
+        //
+        d10.add("1");
+        d10.add("2");
+        d10.add("3");
+        d10.add("4");
+        d10.add("5");
+        d10.add("6");
+        d10.add("7");
+        d10.add("8");
+        d10.add("9");
+        d10.add("10");
+        d10.add("11");
+        d10.add("12");
+        //
+        e10.add("1");
+        e10.add("2");
+        e10.add("3");
+        e10.add("4");
+        e10.add("5");
+        e10.add("6");
+        e10.add("7");
+        e10.add("8");
+        e10.add("9");
+        e10.add("10");
+        e10.add("11");
+        e10.add("12");
+        //
+        f10.add("1");
+        f10.add("2");
+        f10.add("3");
+        f10.add("4");
+        f10.add("5");
+        f10.add("6");
+        f10.add("7");
+        f10.add("8");
+        f10.add("9");
+        f10.add("10");
+        f10.add("11");
+        f10.add("12");
+        //
+        g10.add("1");
+        g10.add("2");
+        g10.add("3");
+        g10.add("4");
+        g10.add("5");
+        g10.add("6");
+        g10.add("7");
+        g10.add("8");
+        g10.add("9");
+        g10.add("10");
+        g10.add("11");
+        g10.add("12");
+        //
+        h10.add("1");
+        h10.add("2");
+        h10.add("3");
+        h10.add("4");
+        h10.add("5");
+        h10.add("6");
+        h10.add("7");
+        h10.add("8");
+        h10.add("9");
+        h10.add("10");
+        h10.add("11");
+        h10.add("12");
+        //
+        j10.add("1");
+        j10.add("2");
+        j10.add("3");
+        j10.add("4");
+        j10.add("5");
+        j10.add("6");
+        j10.add("7");
+        j10.add("8");
+        j10.add("9");
+        j10.add("10");
+        j10.add("11");
+        j10.add("12");
+        
+		
+		
+        Implementation obj = new Implementation();
+		int start = 1;
+		Scanner scan = new Scanner(System.in);
+		while(start==1) {
+			System.out.println("1 TO BOOKING \n2 TO SHOW AVAILABLITY \n3.TO EXIT");
+			int choice = scan.nextInt();   
+			
+			switch(choice) {
+			case 1:
+				System.out.println("BOOKING");
+				
+				obj.booking(a11,b11,c11,d11,e11,f11,g11,h11,j11,  a12,b12,c12,d12,e12,f12,g12,h12,j12,
+						a6,b6,c6,d6,e6,f6,g6,h6,j6,  a10,b10,c10,d10,e10,f10,g10,h10,j10);
+				break;
+				
+			case 2:    
+				System.out.println("SHOW SEAT AVAILABLITY & NEXT SHOW");
+				obj.nextShow(a11,b11,c11,d11,e11,f11,g11,h11,j11,  a12,b12,c12,d12,e12,f12,g12,h12,j12,
+						a6,b6,c6,d6,e6,f6,g6,h6,j6,  a10,b10,c10,d10,e10,f10,g10,h10,j10);
+				break;
+				
+			case 3:
+				start++;
+				break;
+				
+				default:
+					System.out.println("INVALID INPUT");
+			}
+			
+		}
+		
+	}
+	
+
+}
